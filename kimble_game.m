2b1d64e4d1e53d259005effb567efb08c24a6e78
hjpulkki <heikki.j.pulkkinen@gmail.com>
2016-01-16 15:27:46 +0200

    Adds GUI

--- kimble_game.m
+++ kimble_game.m
@@ -5,13 +5,16 @@
 %   goal = 28 to 31
 
 locations = - ones(4);
-player = 1; % index of current player.
+player = 0; % index of current player.
 
 % counter for how many times each player has been eaten.
 hits = zeros(4, 1);
 
+h = figure;
+axis([-1 1 -1 1]);
+
 while true
-    % player = mod(player, 4) + 1;
+    player = mod(player, 4) + 1
     die = CastDie(0);
     locations = UpdateGame(locations, die, player);
     locations
@@ -19,6 +22,7 @@ while true
     % TODO remove possible eatens and update hits.
     % remove to adjust for 
     
+    updateGUI(h, locations);
     % Do we have a winner?
     if min(locations(:, player)> ones(4, 1) * 27) 
         break;
